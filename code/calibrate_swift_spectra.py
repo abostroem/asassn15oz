62ae37e73bfed6c6d7b8891223e49144ce6f6134
Azalee Bostroem <abostroem@gmail.com>
2018-06-20 11:53:54 -0700

    updating to reflect final analysis

--- code/calibrate_swift_spectra.py
+++ code/calibrate_swift_spectra.py
@@ -4,14 +4,14 @@ from argparse import ArgumentParser
 import pickle
 
 import matplotlib
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from astropy.io import fits
 from astropy.io.fits import Column, ColDefs
 
 from uvotpy import uvotgetspec
 from uvotpy.uvotgetspec import getSpec
-from uvotpy.uvotspec import sum_PHAspectra
+#from uvotpy.uvotspec import sum_PHAspectra
 
 
 
@@ -65,81 +65,114 @@ def pickle_output(Y0, Y1, Y2, Y3, Y4, obsid, ext):
     ofile = open('getspec_output_{}_{}.pkl'.format(obsid, ext), 'wb')
     pickle.dump(output_dict, ofile)
 
-def calibrate_spectra(interactive=False, yoffset=None, obs_end='', bkg_lower=[None, None], 
-                      bkg_upper=[None, None]):
-    for obs in observations:
-        if obs != 3:
-            obsid = '{}{:2}'.format(obsid_base, obs).replace(' ', '0')
-            print('=======================\n{}\n=======================\n'.format(obsid))
-            os.chdir(os.path.join(obsid+obs_end, 'uvot', 'image'))
-            flist = glob.glob('*dt.img*')
-            lenticular_flist = glob.glob('sw*uw*.img*')
-            if len(flist) != 0:
-                grism_next = len(fits.open(flist[0]))-1
-                print('EXT = {}'.format(grism_next))
-                for ifile in lenticular_flist:
-                    next = len(fits.open(ifile))-1
-                    if 'uw1' in ifile:
-                        uw1_next = next
-                    else:
-                        uw1_next = None
-                    if 'uw2' in ifile:
-                        uw2_next = next
-                    else:
-                        uw2_next = next
-                pp = PdfPages(os.path.join(FIG_DIR,'{}_plots.pdf'.format(obsid)))
-                for ext in range(grism_next):
-                    if yoffset is not None:
-                        if obs in yoffset.keys():
-                            print('using Y offset')
-                            (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
-                                fit_second=False, 
-                                clobber=True,
-                                optimal_extraction=False,
-                                interactive=True,
-                                background_lower=bkg_lower, 
-                                background_upper=bkg_upper, 
-                                calfile=calfile,
-                                lfilt1_ext=uw1_next,
-                                lfilt2_ext=uw2_next,
-                                offsetlimit=yoffset[obs],
-                                chatter=3)
+def run_getspec(ra, dec, observation, obsid, ext, interactive=False, yoffset=None, bkg_lower=[None, None],
+                bkg_upper=[None, None], bkg_templ=None, uw1_next=None, uw2_next=None):
+    calfile='/Users/bostroem/Desktop/research/not_my_code/uvotpy/calfiles/swugu0160wcal20041120v002.fits'
+    if yoffset is not None:
+        if observation in yoffset.keys():
+            print('using Y offset')
+            (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
+                fit_second=False, 
+                clobber=True,
+                optimal_extraction=False,
+                interactive=True,
+                background_lower=bkg_lower, 
+                background_upper=bkg_upper,
+                background_template=bkg_templ, 
+                calfile=calfile,
+                lfilt1_ext=uw1_next,
+                lfilt2_ext=uw2_next,
+                offsetlimit=yoffset[observation][ext],
+                chatter=3)
 
-                        else:
-                            (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
-                                                            fit_second=False, 
-                                                            clobber=True,
-                                                            optimal_extraction=False,
-                                                            interactive=True,
-                                                            background_lower=bkg_lower, 
-                                                            background_upper=bkg_upper, 
-                                                            calfile=calfile,
-                                                            lfilt1_ext=uw1_next,
-                                                            lfilt2_ext=uw2_next,
-                                                            chatter=3)
-                    else:
-                        (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
-                                                            fit_second=False, 
-                                                            clobber=True,
-                                                            optimal_extraction=False,
-                                                            interactive=True,
-                                                            background_lower=bkg_lower, 
-                                                            background_upper=bkg_upper, 
-                                                            calfile=calfile,
-                                                            lfilt1_ext=uw1_next,
-                                                            lfilt2_ext=uw2_next,
-                                                            chatter=3)
-                    save_background_data(Y0, Y1, obsid, ext+1)
-                    pickle_output(Y0, Y1, Y2, Y3, Y4, obsid, ext+1)
-                    if interactive is True:
-                        raw_input('Press enter to continue')
-                    for i in range(3):
-                        fig = pyplot.figure(i+1)
-                        fig.suptitle('{}'.format(obsid))
-                        pp.savefig(fig)
-                        pyplot.close(fig)
-                pp.close()
-            os.chdir(original_dir)
+        else:
+            (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
+                                            fit_second=False, 
+                                            clobber=True,
+                                            optimal_extraction=False,
+                                            interactive=interactive,
+                                            background_lower=bkg_lower, 
+                                            background_upper=bkg_upper,
+                                            background_template=bkg_templ,  
+                                            calfile=calfile,
+                                            lfilt1_ext=uw1_next,
+                                            lfilt2_ext=uw2_next,
+                                            chatter=3)
+    else:
+        (Y0, Y1, Y2, Y3, Y4) = getSpec(ra, dec, obsid, ext+1, 
+                                            fit_second=False, 
+                                            clobber=True,
+                                            optimal_extraction=False,
+                                            interactive=interactive,
+                                            background_lower=bkg_lower, 
+                                            background_upper=bkg_upper, 
+                                            background_template=bkg_templ,
+                                            calfile=calfile,
+                                            lfilt1_ext=uw1_next,
+                                            lfilt2_ext=uw2_next,
+                                            chatter=3)
+    return Y0, Y1, Y2, Y3, Y4
+    
+
+def get_lenticular_images(obsid, data_dir):
+    '''
+    Get number of extensions in lenticular images
+    '''
+    lenticular_flist = glob.glob(os.path.join(data_dir, obsid, 'uvot', 'image', 'sw*uw*.img*'))
+    #Loop over lenticular images
+    if len(lenticular_flist) == 0:
+        print('WARNING: No lenticular files found in {}'.format(os.path.join(data_dir, obsid, 'uvot', 'image')))
+        return None, None
+    for ifile in lenticular_flist:
+        next = len(fits.open(ifile))-1
+        if 'uw1' in ifile:
+            uw1_next = next
+        else:
+            uw1_next = None
+        if 'uw2' in ifile:
+            uw2_next = next
+        else:
+            uw2_next = None
+    return uw1_next, uw2_next
+
+def calibrate_spectra(interactive=False, yoffset=None, obs_end='', bkg_lower=[None, None], 
+                      bkg_upper=[None, None], bkg_templ=None):
+    #Loop over visits
+    for obs in observations:
+        obsid = '{}{:2}'.format(obsid_base, obs).replace(' ', '0')
+        uw1_next, uw2_next = get_lenticular_images(obsid, DATA_DIR)
+        print('=======================\n{}\n=======================\n'.format(obsid))
+        os.chdir(os.path.join(obsid+obs_end, 'uvot', 'image'))
+        flist = glob.glob('*dt.img*')
+        #If grism files exist
+        if len(flist) != 0:
+            grism_next = len(fits.open(flist[0]))-1
+            print('EXT = {}'.format(grism_next))
+            #Loop over lenticular images
+            
+            pp = PdfPages(os.path.join(FIG_DIR,'{}_plots.pdf'.format(obsid)))
+            #Loop over grism extensions
+            for ext in range(grism_next):
+                Y0, Y1, Y2, Y3, Y4 = run_getspec(ra, dec, obs, obsid, ext, 
+                                                 interactive=interactive, 
+                                                 yoffset=yoffset, 
+                                                 bkg_lower=[None, None],
+                                                 bkg_upper=[None, None], 
+                                                 bkg_templ=None,
+                                                 uw1_next=uw1_next, 
+                                                 uw2_next = uw2_next)
+                save_background_data(Y0, Y1, obsid, ext+1)
+                pickle_output(Y0, Y1, Y2, Y3, Y4, obsid, ext+1)
+                if interactive is True:
+                    raw_input('Press enter to continue')
+                for i in range(3):
+                    fig = plt.figure(i+1)
+                    fig.suptitle('{}'.format(obsid))
+                    pp.savefig(fig)
+                    plt.close(fig)
+            pp.close()
+        os.chdir(original_dir)
+        
 def combine_spectra(obs_end=''):
     for obs in observations:
         if obs != 3:
@@ -156,16 +189,18 @@ if __name__ == "__main__":
                         help='--show \t [%(default)s], if set interactive plots are displayed')
     args = parser.parse_args()
         
+    if args.show:
+        plt.ion()
     ra = 289.889800 
     dec = -33.767000
-
-    calfile='/Users/bostroem/Desktop/research/not_my_code/uvotpy/calfiles/swugu0160wcal20041120v002.fits'
 
     obsid_base = '000340400'
     #observations = range(1, 12)
     #observations=[1, 5, 9]
-    observations=[2, 15]
-    os.chdir('../data/swiftuvot/reduced')
+    observations=[1, 2] #First visit, only one with extractable signal
+    #os.chdir('../data/swiftuvot/reduced_default')
+    DATA_DIR = './'
+    #os.chdir('../data/swiftuvot/reduced_center_zeroth')
 
     original_dir = os.getcwd()
     check_environment()
@@ -174,11 +209,22 @@ if __name__ == "__main__":
     #Default extraction parameters
     #Template matches first extraction
     #--------------------------------
-    FIG_DIR = '../../../../../../figures'
+#     FIG_DIR = '../../../../../../figures'
+#     FIG_DIR = './'
+#     yoffset={1:[[91,1], [91,1], [91,1]], 2:[[80,1], [93,0], [89, 0]]}
+#     calibrate_spectra(interactive=args.show, yoffset=yoffset)
+    #combine_spectra()
+    #--------------------------------
+    #Test TRUVOT against original extraction
+    #Test default extraction
+    # No need for y-offsets, default extraction works well
+    #--------------------------------
+    os.chdir('../data/swiftuvot/test_truvot_extraction/original') 
     FIG_DIR = './'
-    yoffset={15:[109,0]}
-    calibrate_spectra(interactive=args.show, yoffset=yoffset)
-    combine_spectra()
+    calibrate_spectra(interactive=args.show, yoffset=None)
+    #combine_spectra()
+    
+    
     #--------------------------------
     #Offsets to exclude star just above spectrum at PA=248
     #--------------------------------
