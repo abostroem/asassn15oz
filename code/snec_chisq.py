388f3071ad59332ce71bc8ba6829cc51d3bdc33f
Azalee Bostroem <abostroem@gmail.com>
2019-01-04 18:11:03 -0800

    adding an if name == main statement

--- code/snec_chisq.py
+++ code/snec_chisq.py
@@ -21,18 +21,162 @@ SAVE_DIR = '../snec_models'
 
 snec_models = os.path.join(DARK_DIR,'SNEC/snec_models/')
 
-filters_1 = ['U', 'B', 'V', 'R', 'I']
-filters_2 = ['V', 'R', 'I']
-end_uv = 30
-end_plateau = 150
+def calc_abs_mag(app_mag, dist_mod, A_mw, A_host=0, dist_mod_err=0, app_mag_err=0, A_err_mw=0, A_err_host=0):
+    '''
+    Calculate the absolute magnitude in a given filter given the apparent magnitude, distance modulus, and extinction
+    app_mag: arr or float
+        array of apparent magnitudes
+    dist_mod: float
+        distance modulus
+    A_mw: float
+        Magnitudes of extinction in filter due to the Milky Way
+    A_host: float
+        Magnitudes of extinction in filter due to host; default=0
+    dist_mod_err: float
+        error in the distance modulus; default=0
+    app_mag_err: arr or float
+        array of error values for apparent magnitude (should be same size as app_mag); default=0
+    A_err_mw: float
+        error on A_mw (in magnitudes); default=0
+    A_err_host: float
+        err on A_host (in magnitudes); default=0
+    '''
+    abs_mag = app_mag - dist_mod - A_mw - A_host
+    abs_mag_err = np.sqrt(app_mag_err**2 + dist_mod_err**2 + A_err_mw**2 + A_err_host**2)
+    return abs_mag, abs_mag_err
 
-ni_mass =   np.array([0.083, 0.0965, 0.11])
-energies =  np.array([0.5, 0.8, 1.1, 1.4, 1.7, 2.0])
-masses =    np.array([11, 13, 15, 16, 17, 18, 21])
-ni_mixing = np.array([5.0])
-time_offsets = np.arange(-4, 4, 1)
-Kvalues =   np.array([0, 10, 20, 30, 35, 40, 50, 60])
-radii =     np.array([0, 1500, 1800, 2100, 2400, 2700, 3000, 3300])
+class LightCurve2(object):
+    '''
+    Retrieve information from sndavis database
+    
+    Absolute magnitudes are corrected for extinction, apparent magnitude is not
+    '''
+    def __init__(self, name):
+        self.name=name
+        self.db, self.cursor = connect_to_sndavis.get_cursor()
+        self.id = self.get_sn_id()
+        if self.id is not None:
+            self.cursor.execute('SELECT * FROM idsupernovae WHERE id = {}'.format(self.id))
+            result = self.cursor.fetchone()
+            self.ra = result['ra0']
+            self.dec = result['dec0']
+            self.type = result['sntype'] #TODO - update this to look at the type table and make a human readable type
+            self.dist_mod = result['mu']
+            self.dist_mod_err = result['muerr']
+            self.ebv_mw = result['ebvg']
+            self.ebv_mw_err = result['ebvgerr']
+            self.ebv_host = result['ebvi'] 
+            self.ebv_host_err = result['ebvierr']
+            self.quality = result['quality']
+            self.jdexpl = result['jdexpl']
+            self.jdexpl_err = result['jdexplerr']
+            self.jd = {}
+            self.apparent_mag = {}
+            self.apparent_mag_err = {}
+            self.phase = {}
+            self.abs_mag = {}
+            self.abs_mag_err = {}
+            self.slopes = {'s1':{}, 's2':{}, 's50':{}, 'tail':{}, 
+                            's1_range':{}, 's2_range':{}, 's50_range':{}, 'tail_range':{},
+                            's1_err':{}, 's2_err':{}, 's50_err':{}, 'tail_err':{}}
+            self.A_host = {}
+            self.A_err_host = {}
+            self.A_mw = {}
+            self.A_err_mw = {}
+        else:
+            print('SN {} not found in database'.format(self.name))
+            #TODO add an absolute magnitude error
+             
+    def get_sn_id(self):
+        self.cursor.execute('SELECT targetid FROM supernovanames WHERE name = {}'.format('"{}"'.format(self.name)))
+        result = self.cursor.fetchone()
+        if result is None:
+            return None
+        return result['targetid']
+ 
+    def get_photometry(self, band='all'):
+        '''
+        Get apparent magnitude of a specific band
+        '''
+        if band == 'all':
+            self.cursor.execute("SELECT DISTINCT BINARY(filter) FROM photometry WHERE targetid={}".format(self.id))
+            results = self.cursor.fetchall()
+            bands = [iband['BINARY(filter)'].decode('utf-8') for iband in results]
+
+        elif len(band)==1: #Single filter
+            bands = [band]
+        else: #multiple filters
+            bands = band
+        for ifilter in bands:   
+            A_host_band, A_err_host_band = spec.calc_extinction(self.ebv_host, ifilter)
+            A_mw_band, A_err_mw_band = spec.calc_extinction(self.ebv_mw, ifilter)
+            self.cursor.execute("SELECT jd, mag, magerr FROM photometry WHERE targetid={} AND filter=BINARY('{}')".format(self.id, ifilter))
+            results = self.cursor.fetchall()
+            jd = []
+            mag = []
+            mag_err = []
+            for irow in results:
+                jd.append(float(irow['jd']))
+                mag.append(float(irow['mag']))
+                mag_err.append(float(irow['magerr']))
+            self.jd[ifilter] = np.array(jd)
+            self.apparent_mag[ifilter] = np.array(mag)
+            self.apparent_mag_err[ifilter] = np.array(mag_err)
+            self.phase[ifilter] = self.jd[ifilter] - self.jdexpl
+            self.A_host[ifilter] = A_host_band
+            self.A_err_host[ifilter] = A_err_host_band
+            self.A_mw[ifilter] = A_mw_band
+            self.A_err_mw[ifilter] = A_err_mw_band
+            
+    def get_abs_mag(self, band='all'):
+        '''
+        Calcualte absolute magnitude
+        '''
+        print('Calculating Absolute Magntidue with Extinction')
+        if band == 'all':
+            bands = self.apparent_mag.keys()
+        elif len(band)==1:
+            bands = [band]
+        else:
+            bands = band
+        if self.dist_mod_err is None:
+            self.dist_mod_err = 0
+        for iband in bands:
+            if self.ebv_host is None:
+                self.abs_mag[iband], self.abs_mag_err[iband] = calc_abs_mag(self.apparent_mag[iband], 
+                                                   self.dist_mod, 
+                                                   self.A_mw[iband], 
+                                                   app_mag_err=self.apparent_mag_err[iband], 
+                                                   dist_mod_err=self.dist_mod_err)
+            else:
+                self.abs_mag[iband], self.abs_mag_err[iband] = calc_abs_mag(self.apparent_mag[iband], 
+                                                                            self.dist_mod, 
+                                                                            self.A_mw[iband], 
+                                                                            A_host=self.A_host[iband],
+                                                                            app_mag_err=self.apparent_mag_err[iband], 
+                                                                            dist_mod_err=self.dist_mod_err,
+                                                                            A_err_mw=self.A_err_mw[iband],
+                                                                            A_err_host=self.A_err_host[iband])
+    
+    def get_slope(self,slope_type, band='V'):
+        '''
+        '''
+        self.cursor.execute('SELECT * FROM snslope WHERE  targetid={} AND \
+                                                         slopetype="{}" AND \
+                                                         filter=BINARY("{}")'.format(self.id, slope_type, band))
+        result = self.cursor.fetchone()
+        if result is not None:
+            self.slopes['{}'.format(slope_type)][band]       = result['slope']
+            self.slopes['{}_err'.format(slope_type)][band]   = result['slopeerr']
+            self.slopes['{}_range'.format(slope_type)][band] = (result['tstart'], result['tstop'])
+        else:
+            self.slopes['{}'.format(slope_type)][band]       = None
+            self.slopes['{}_err'.format(slope_type)][band]   = None
+            self.slopes['{}_range'.format(slope_type)][band] = [None, None]
+
+
+
+
 
 def prepare_model_data(model_dir):
     model_mag_tbdata = asc.read(os.path.join(model_dir,'magnitudes.dat'),
@@ -280,4 +424,20 @@ def calc_color_chisq():
     #ax.plot(1, 4, 'w*')
     fig.colorbar(im)
     plt.savefig(os.path.join(FIG_DIR, 'chisq_color_KvsR.pdf'))
+
+if __name__ == "__main__":
+    filters_1 = ['U', 'B', 'V', 'R', 'I']
+    filters_2 = ['V', 'R', 'I']
+    end_uv = 30
+    end_plateau = 150
+
+    ni_mass =   np.array([0.083, 0.0965, 0.11])
+    energies =  np.array([0.5, 0.8, 1.1, 1.4, 1.7, 2.0])
+    masses =    np.array([11, 13, 15, 16, 17, 18, 21])
+    ni_mixing = np.array([5.0])
+    time_offsets = np.arange(-4, 4, 1)
+    Kvalues =   np.array([0, 10, 20, 30, 35, 40, 50, 60])
+    radii =     np.array([0, 1500, 1800, 2100, 2400, 2700, 3000, 3300])
+    calc_color_chisq()
+    calculate_bolometric_chisq()
     
